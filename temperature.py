eda634347b848463c5301286151e7bf47693f234
devanshig-13 <guptadevanshi2007@gmail.com>
2026-02-24 13:04:28 +0530

    Add files via upload

---
+++ temperature.py
@@ -0,0 +1,20 @@
+import random
+import time
+
+min_temp = int(input("Enter minimum temperature: "))
+max_temp = int(input("Enter maximum temperature: "))
+
+
+for i in range(10):  
+    temp = random.randint(0, 100) 
+    
+    print("Temperature:", temp)
+    
+    if temp > max_temp:
+        print("Alert: Temperature is too high")
+    elif temp < min_temp:
+        print("Alert: Temperature is too low")
+    else:
+        print("Temperature is within acceptable limit")
+    
+    time.sleep(2) 
